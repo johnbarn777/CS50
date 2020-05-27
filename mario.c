9bd18fd7236d1f37f8df9ea014b846976d999842
johnbarn777 <johnbarn777@users.noreply.github.com>
2020-05-27 11:32:57 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -13,19 +13,23 @@ int main(void)
     while (n <1 || n >8);
   for(int i=0;i<n;i++)
   
-  {int m= i;
+  {int m=i;
+   
   for(int k=n-m;k>1;k--)
-  {
+  {// this loop is for spaces before pyramid
       printf(" ");
   }
       for(int j=0;j<=m;j++)
-      {
+      {//this one for first half of pyramid
           printf("#");
-      }printf("  ");
+      }
+      printf("  ");
+      
       for(int l=0;l<=m;l++)
-      {
+      {// this one for second
           printf("#");
-      }printf("\n");
+      }
+      printf("\n");
   }
   
 }
