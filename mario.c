5f625146116a6d7b627a0342626b6e117412ce44
johnbarn777 <johnbarn777@users.noreply.github.com>
2020-05-27 11:14:11 +0000

    automated commit by check50 [check50=True]

---
+++ mario.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <cs50.h>
+
+
+int n;
+int main(void)
+{
+  do
+  {
+      n =get_int("Height: ");
+  }
+  while(n<1);
+  for(int i=0;i<n;i++)
+  
+  {int m= i;
+  for(int k=n-m;k>0;k--)
+  {
+      printf(" ");
+  }
+      for(int j=0;j<=m;j++)
+      {
+          printf("#");
+      }printf(" ");
+      for(int l=0;l<=m;l++)
+      {
+          printf("#");
+      }printf("\n");
+  }
+  
+}
