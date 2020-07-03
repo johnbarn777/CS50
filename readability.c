6edd8972a93fae5b57cbe80b265246a7add8ba58
johnbarn777 <johnbarn777@users.noreply.github.com>
2020-07-03 10:39:40 +0000

    automated commit by check50 [check50=True]

---
+++ readability.c
@@ -0,0 +1,47 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+
+int main(void)
+{
+    string text = get_string("Text :");
+    int letters=0;
+    int words=1;
+    int sentences=0;
+    for (int i=1; i <=strlen(text); i++)
+    {
+        if((64<text[i] && text[i]<91) || (96<text[i] && text[i]<123))
+        {
+            letters++;
+            
+        }
+        if(text[i]==32)
+        {
+            words++;
+        }
+        if(text[i]== 46||text[i]==63||text[i]==33)
+        {
+            sentences++;
+        }
+       
+    }
+     float L = (float)letters*100/words;
+        float S =(float)sentences*100/words;
+        float index = 0.0588 * L - 0.296 * S - 15.8;
+        
+        if(1<index &&index <16)
+        {
+        printf("Grade %.f\n",index);
+        }
+        if(index<1)
+        {
+            printf("Before Grade 1\n");
+        }
+        if(index>=16)
+        {
+            printf("Grade 16+\n");
+        }
+    
+}
