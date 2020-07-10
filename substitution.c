3b94995045deb74c40dec17e73658119822133e0
johnbarn777 <johnbarn777@users.noreply.github.com>
2020-07-10 08:20:48 +0000

    automated commit by check50 [check50=True]

--- substitution.c
+++ substitution.c
@@ -9,6 +9,7 @@ int main(int argc, string argv[])
     if(argc != 2 )
     {
          printf("Usage : ./substitution key\n");
+         return 1;
     }
     for(int i=0; i<strlen(argv[1]); i++)
     {
